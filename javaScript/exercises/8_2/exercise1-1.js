d7969e053bb99dfee0bd3cffcb0ed3ae3a4695d3
Jafet Henrique <jafetguerra6@gmail.com>
2020-03-19 16:07:31 -0300

    Exercises about Object Manipulation in ES6

---
+++ javaScript/exercises/8_2/exercise1-1.js
@@ -0,0 +1,20 @@
+const customer1 = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+customer1.lastName = 'Faria';
+customer1.nickName = "Fafa"
+console.log(customer1);
+
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
+
+
+customer2['lastName'] = 'Silva';
+customer2['nickName'] = 'Mama'
+console.log(customer2);
