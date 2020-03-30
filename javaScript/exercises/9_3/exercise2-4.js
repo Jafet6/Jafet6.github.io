c36ba383a08a5101a1c69a6d6ce1bf90aafc2580
Jafet Henrique <jafetguerra6@gmail.com>
2020-03-30 19:07:52 -0300

    Exercises about Spread Operator, Rest, Destructuring

---
+++ javaScript/exercises/9_3/exercise2-4.js
@@ -0,0 +1,44 @@
+const assert = require('assert')
+
+const people = [
+    {
+        name: "Nicole",
+        bornIn: 1992,
+        nationality: "Australian"
+    },
+    {
+        name: "Harry",
+        bornIn: 2008,
+        nationality: "Australian"
+    },
+    {
+        name: "Toby",
+        bornIn: 1901,
+        nationality: "Australian"
+    },
+    {
+        name: "Frida",
+        bornIn: 1960,
+        nationality: "Dannish"
+    },
+    {
+        name: "Fernando",
+        bornIn: 2001,
+        nationality: "Brazilian"
+    }
+]
+
+// escreva filterPeople abaixo
+
+const filterPeople = (arrPeople) => {
+  return arrPeople.filter(element => element.nationality === 'Australian' && element.bornIn < 2000);
+}
+
+const filteredPeople = filterPeople(people);
+
+
+//console.log(filteredPeople[0]);
+
+assert.deepEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
+    
