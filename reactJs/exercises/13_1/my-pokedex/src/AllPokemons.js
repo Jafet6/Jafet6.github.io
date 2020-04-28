a962e170fc9d24869118345b6846c9444b51c6ae
Jafet Henrique <jafetguerra6@gmail.com>
2020-04-28 13:15:01 -0300

    my pokedex

---
+++ reactJs/exercises/13_1/my-pokedex/src/AllPokemons.js
@@ -0,0 +1,6 @@
+
+
+
+<div className='pokedex-cointainer'>
+  {pokemons.map(e => <Pokemon key={e.id} pokes={e} />)}
+</div>
---
+++ reactJs/exercises/13_1/my-pokedex/src/Pokedex.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import Pokemon from './Pokemon';
+import pokemons from './data';
+import './Pokemon.css'
+
+class MyButton extends React.Component {
+  render() {
+    return (
+      <button onClick={() => this.props.handleClick()}>{this.props.label}</button>
+    )
+  }
+}
+
+class Pokedex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      length: pokemons.filter(e => e.type === 'Psychic').length,
+      index: 0,
+      pokemon: '',
+      type: 'Psychic',
+    }
+  }
+
+  
+  setPsychicPokemon = () => {
+    const arrLength = pokemons.filter(e => e.type === 'Psychic').length;
+    this.setState({
+      length: arrLength,
+      type: 'Psychic',
+    })
+  }
+  
+  setFirePokemon = () => {
+    const arrLength = pokemons.filter(e => e.type === 'Fire').length;
+    this.setState({
+      length: arrLength,
+      type: 'Fire',
+    })
+  }
+  
+  setNextPokemon = () => {
+    if(this.state.index >= this.state.length - 1) {
+      return this.setState({index: 0});
+    }
+    return this.setState((state) => ({
+      index: state.index + 1,
+    }))
+  }
+
+  setAllPokemons = () => {
+    this.setState({
+      type: '',
+      length: pokemons.length,
+  })
+  }
+  
+  render() {
+    const arrSelected = () => {
+      if (this.state.type !== '') {
+        const selectedPokemon = pokemons.filter(e => e.type === this.state.type);
+        return selectedPokemon;
+      }
+      return pokemons;
+    }
+    console.log(this.state.type)
+    console.log(this.state.length)
+    return (
+      <div>
+        <h1>Pokedex</h1>
+        <div>
+          <div className='pokedex-cointainer'>
+            <Pokemon pokes={arrSelected()[this.state.index]} />
+          </div>
+          <MyButton label={'Próximo Pokemon'} handleClick={this.setNextPokemon}/>
+          <MyButton label={'Psychic'} handleClick={this.setPsychicPokemon} />
+          <MyButton label={'Fire'} handleClick={this.setFirePokemon} />
+          <MyButton label={'All'} handleClick={this.setAllPokemons} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Pokedex;
