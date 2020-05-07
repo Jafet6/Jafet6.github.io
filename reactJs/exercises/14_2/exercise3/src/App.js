9bfc5a140b0090669c2abb6456b7b31bdce0a570
Jafet Henrique <jafetguerra6@gmail.com>
2020-05-07 13:19:50 -0300

    Bonus exercises about React Lifecycle Components

--- reactJs/exercises/14_2/exercise3/src/App.js
+++ reactJs/exercises/14_2/exercise3/src/App.js
@@ -7,14 +7,18 @@ class App extends React.Component {
     super(props);
     this.state = {
       dogURL: '',
+      name: '',
       isLoading: null,
       error: null,
       breed: '',
+      arrDog: [],
     };
-    this.componentDidMount = this.componentDidMount.bind(this);
+    this.apiDog = this.apiDog.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
+  apiDog() {
     this.setState({ isLoading: true }, () => {
       fetch('https://dog.ceo/api/breeds/image/random')
         .then((resp) => resp.json())
@@ -32,26 +36,56 @@ class App extends React.Component {
               error 
             })
           }
-        )
-
+      )
     })
   }
 
+  componentDidMount() {
+    if (localStorage.getItem('arrDog')) {
+      const arrDog = JSON.parse(localStorage.getItem("arrDog"));
+      this.setState({
+        dogURL: arrDog[0].dogURL,
+        name: arrDog[0].name,
+      })
+    } else {
+      this.apiDog();
+    }
+
+  }
+
   componentDidUpdate(prevProps, prevState) {
-    localStorage.setItem('lastUrl', prevState.dogURL)
-    if (prevState.breed !== this.state.breed) return alert(`Raça: ${this.state.breed}`)
+    const { arrDog, breed } = this.state;
+    if (arrDog.length > 0) localStorage.setItem('arrDog', JSON.stringify(arrDog));
+    if (prevState.breed !== breed) return alert(`Raça: ${breed}`)
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value, })
+  }
+
+  handleClick() {
+    const { name, dogURL, arrDog } = this.state;
+    this.setState({ 
+      arrDog: [...arrDog, {name, dogURL}],
+     })
   }
 
   render() {
-    const { dogURL, isLoading } = this.state;
+    const { dogURL, isLoading, name } = this.state;
     return (
       <div>
         Troque o Doguinho
         <button
-          onClick={this.componentDidMount}>
+          onClick={this.apiDog}>
           Novo Doguinho
-        </button>
+        </button><br></br>
+        <span>{`Nome: ${name}`}</span>
         <DogImg dogURL={dogURL} isLoading={isLoading} loading="Loading..." />
+        <label>
+          Nome:
+          <input type="text" onChange={this.handleChange} value={name} ></input>
+        </label><br></br>
+        <button type="button" onClick={this.handleClick}>Salvar</button>
       </div>
     );
   }
