1854cc2d9cebc80cc9beb362db53e44957687c5b
Jafet Henrique <jafetguerra6@gmail.com>
2020-05-28 13:50:40 -0300

    Bonus exercise about React Redux

--- reactJs/exercises/17_3/exercise2/src/components/RegisteredClients.js
+++ reactJs/exercises/17_3/exercise2/src/components/RegisteredClients.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const RegisteredClients = (props) => {
-  console.log(props)
-  const { clientsList, loged } = props;
-  if(loged) {
+class RegisteredClients extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrClients: this.props.clientsList,
+      sorted: false,
+    }
+  }
+
+  sortNames = () => {
+    const { sorted } = this.state;
+    const copiedArr = [...this.props.clientsList]
+    const newOrderedList = copiedArr.sort((a, b) =>(a.name > b.name) ? 1 : -1);
+    if(!sorted) {
+      return this.setState({ arrClients: newOrderedList, sorted: true, });
+    }
+    return this.setState({ arrClients: this.props.clientsList, sorted: false})
+  }
+
+  render() {
+    const { loged } = this.props;
+    const { arrClients } = this.state;
+    if(loged) {
+      return (
+        <div>
+          <h2>Clientes cadastrados</h2>
+          <ul>
+            {arrClients.map((client, index) => 
+              (<li key={index}>{`Nome: ${client.name} Idade: ${client.age}`}</li>))
+            }
+          </ul>
+          <button onClick={this.sortNames} type="button">Ordenar Nomes</button>
+        </div>
+      )
+    }
     return (
-      <div>
-        <h2>Clientes cadastrados</h2>
-        <ul>
-          {clientsList.map((client) => 
-            (<li key={client.email}>{`Nome: ${client.name} Idade: ${client.age}`}</li>))
-          }
-        </ul>
-      </div>
+      <h2>Login não efetuado</h2>
     )
   }
-  return (
-    <h2>Login não efetuado</h2>
-  )
+  
 }
 
 const mapStateToProps = (state) => ({
