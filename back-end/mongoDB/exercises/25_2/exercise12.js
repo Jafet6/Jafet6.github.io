351e356c6da20db707fc5ae5aaf27951eab92779
Jafet Henrique <jafetguerra6@gmail.com>
2020-08-10 20:35:57 -0300

    exercise about aggregate framework MongoDb

---
+++ back-end/mongoDB/exercises/25_2/exercise12.js
@@ -0,0 +1,44 @@
+db.vendas.aggregate([
+  {
+    $match: {
+      "dataVenda": {
+        $gte: ISODate('2020-03-01'),
+        $lte: ISODate('2020-03-31')
+      },
+      "status": "EM SEPARACAO"
+    }
+  }, 
+  {
+    $addFields: {
+      "dataEntregaPrevista": { $add: [ "$dataVenda", 3 * 24 * 60 * 60 * 1000 ] }
+    }
+  },
+  {
+    $project: {
+      "_id": 0,
+      "clienteId": 1,
+      "dataVenda": 1,
+      "dataEntregaPrevista": 1
+    }
+  },
+  {
+    $group: {
+      "_id": null,
+      "primeiraEntrega": { $min: "$dataEntregaPrevista" },
+      "ultimaEntrega": { $max: "$dataEntregaPrevista" }
+    }
+  },
+  {
+    $project: {
+      "_id": 0,
+      "diasDif": {
+        $ceil: {
+          $divide: [
+            { $subtract: [ "$ultimaEntrega", "$primeiraEntrega" ] },
+            86400000
+          ]
+        }
+      }
+    }
+  }
+]);
---
+++ back-end/mongoDB/exercises/25_2/exercise3.js
@@ -0,0 +1,27 @@
+db.clientes.aggregate([
+  {
+    $addFields: {
+      "idade": {
+        $floor: {
+          $divide: [
+            { $subtract: [ "$$NOW", "$dataNascimento" ] },
+            { $multiply: [ 86400000, 365 ] }
+          ]
+        }
+      }
+    }
+  },
+  {
+    $match: {
+      "idade": { $gte: 18, $lte: 25 }
+    }
+  },
+  {
+    $lookup: {
+      from: "vendas",
+      localField: "clienteId",
+      foreignField: "clienteId",
+      as: "compras"
+    }
+  }
+]);
