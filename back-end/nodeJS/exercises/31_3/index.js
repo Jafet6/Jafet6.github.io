dbf2270da21e55ed2fb52ab36431fb7b4969923d
Jafet Henrique <jafetguerra6@gmail.com>
2020-10-10 12:31:54 -0300

    exercises bonus about sequelize associations

--- back-end/nodeJS/exercises/31_3/index.js
+++ back-end/nodeJS/exercises/31_3/index.js
@@ -13,6 +13,15 @@ app.get('/patients', async (req, res) => {
 });
 
 app.get('/patients/surgeries', async (req, res) => {
+  const { withoutDoctor } = req.query;
+
+  if (withoutDoctor) {
+    const patients = await Patients.findAll({ include: [{
+      model: Surgeries, as: 'surgeries', attributes: { exclude: ['doctor'] }
+    }] });
+    return res.status(200).json(patients);
+  }
+
   const patients = await Patients.findAll({ include: [{ model: Surgeries, as: 'surgeries'}] });
   return res.status(200).json(patients);
 });
@@ -23,6 +32,17 @@ app.get('/patients/plan/:id', async (req, res) => {
   return res.status(200).json(patients)
 })
 
+app.post('/patients', async (req, res) => {
+  const { fullname, plan_id } = req.body;
+  const patient = await Patients.create({ fullname, plan_id });
+  return res.status(200).json(patient)
+});
+
+app.get('/surgeries/:name', async (req, res) => {
+  const { name } = req.params;
+  const surgeries = await Surgeries.findAll({ where: { doctor: name } })
+  return res.status(200).json(surgeries)
+});
 
 const PORT = 3000;
 
